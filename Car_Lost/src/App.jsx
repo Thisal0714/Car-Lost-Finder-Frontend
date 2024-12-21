e75021d8abdd3fe68581f93517ba613073a969d1
Hasintha-HGP <phasintha34@gmail.com>
2024-12-21 16:54:49 +0530

    add

--- Car_Lost/src/App.jsx
+++ Car_Lost/src/App.jsx
@@ -1,14 +1,14 @@
 import Home from './Commponent/Home/Home.jsx';
 import About from './Commponent/About/About.jsx';
 import './App.css'
-import Profile from './Commponent/Profile/Profile.jsx';
+
 
 function App() {
   
 
   return (
     <>
-      <Profile/>
+      <Home/>
     </>
   )
 }
--- Car_Lost/src/Commponent/Home/Home.jsx
+++ Car_Lost/src/Commponent/Home/Home.jsx
@@ -2,22 +2,22 @@ import './HomeCss.css'
 
 function Home(){
 return(
-<body className='Home'>    
-<div>
-    <div className="home">
-    <h1><b>CAR FINDER</b></h1>
-    <h3>~ CONNECTING YOU WITH YOUR LOST RIDE ~</h3>
+    <body className='Home'>    
+    <div>
+        <div className="home">
+        <h1><b>CAR FINDER</b></h1>
+        <h3>~ CONNECTING YOU WITH YOUR LOST RIDE ~</h3>
+        </div>
+        <div className="Buttons">
+         
+        <button >Sign Up</button>
+        
+          
+        <button >Login</button>
+        
+        </div>
     </div>
-    <div className="Buttons">
-     
-    <button >Sign Up</button>
-    
-      
-    <button >Login</button>
-    
-    </div>
-</div>
-</body>
+    </body>
     );
 }
 
